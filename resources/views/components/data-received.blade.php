fdb89c3190ad844fafaa7b5cc0e2a89dcc7fc01f
Taylor, Daniel <daniel.taylor@nuance.com>
2022-04-11 17:25:57 +0100

    Colours

--- resources/views/components/data-received.blade.php
+++ resources/views/components/data-received.blade.php
@@ -1,7 +1,7 @@
 <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
     <dl class="sm:divide-y sm:divide-gray-200">
       <div class="p-4 sm:py-5 flex sm:gap-4 sm:px-6">
-        <dt class="flex-initial w-32 text-sm font-medium text-gray-500">Return Code</dt>
+        <dt class="flex-initial w-32 text-sm font-bold text-indigo-700">Return Code</dt>
         <dd class="flex-1 mt-1 text-sm text-gray-900 sm:mt-0">
           {{ $event->value->returnCode ?? '' }}
           @if( isset( $event->value->returnCode ) )
@@ -18,11 +18,11 @@
         </dd>
       </div>
       <div class="p-4 sm:py-5 flex sm:gap-4 sm:px-6">
-        <dt class="flex-initial w-32 text-sm font-medium text-gray-500">Duration</dt>
+        <dt class="flex-initial w-32 text-sm font-bold text-indigo-700">Duration</dt>
         <dd class="flex-1 mt-1 text-sm text-gray-900 sm:mt-0">{{ $event->value->duration ?? '' }}</dd>
       </div>
       <div class="p-4 sm:py-5 flex sm:gap-4 sm:px-6">
-        <dt class="flex-initial w-32 text-sm font-medium text-gray-500">Data</dt>
+        <dt class="flex-initial w-32 text-sm font-bold text-indigo-700">Data</dt>
         <dd class="flex-1 mt-1 text-sm text-gray-900 sm:mt-0"><textarea rows="10" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">{{ !empty( $event->value->data ) ? json_encode($event->value->data, JSON_PRETTY_PRINT) : '' }}</textarea></dd>
       </div>
     </dl>
