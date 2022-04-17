54140cf1be58a84c3a7037f597840fe84884facc
Taylor, Daniel <daniel.taylor@nuance.com>
2022-04-17 09:39:20 +0100

    force restart queue

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,6 +28,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         // $schedule->command('testcommand')->everyMinute();
         $schedule->command('cleanup')->dailyAt('00:00');
+        
     }
 
     protected function shortSchedule(ShortSchedule $shortSchedule)
