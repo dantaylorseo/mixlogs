e461dee0bd1ee657f416bb9c9dc8023c63367d59
Taylor, Daniel <daniel.taylor@nuance.com>
2022-07-26 14:26:12 +0100

    Logging

--- app/Services/MixLogService.php
+++ app/Services/MixLogService.php
@@ -441,7 +441,7 @@ class MixLogService
         $project_id = "n/a";
 
         // info("Processing " . count($logs) . " logs");
-        info("(" . $this->application->name . ") Looping " . $logs->count() . " rows");
+        info("(" . $this->application->name . ") - Looping " . count($logs) . " rows");
         foreach ($logs as $log) {
             // if(Carbon::parse($log['value']['timestamp'])->isBefore(Carbon::now()->startOfDay())) continue;
             $logArray[] = [
@@ -502,9 +502,9 @@ class MixLogService
         try {
             
 
-            info("(" . $this->application->name . ") Inserting " . count($logArray) . " rows");
+            info("(" . $this->application->name . ") - Inserting " . count($logArray) . " rows");
             DB::table('logs')->upsert( $logArray, 'id' );
-            info("(" . $this->application->name . ") Inserted " . count($logArray) . " rows");
+            info("(" . $this->application->name . ") - Inserted " . count($logArray) . " rows");
 
             // DB::table('logs')->upsert($logArray, 'id');
             
